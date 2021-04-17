848f25bc0f4ca94903df75169e16139eeb75e90a
Jose Lopez <lopezarizajosealejandro@gmail.com>
2021-04-16 23:21:46 -0500

    Revert "Views"
    This reverts commit 39fbb74ac16f5411c4ccd48c1e5bd6806c271c26.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,24 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './components/login/login.component';
-import { ActivitiesComponent } from './components/activities/activities.component';
-import { GroupsComponent } from './components/groups/groups.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { NavbarVerComponent } from './components/navbar-ver/navbar-ver.component';
-import { SettingComponent } from './components/setting/setting.component';
-import { ShopComponent } from './components/shop/shop.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
-    ActivitiesComponent,
-    GroupsComponent,
-    NavbarComponent,
-    NavbarVerComponent,
-    SettingComponent,
-    ShopComponent
+    LoginComponent
   ],
   imports: [
     BrowserModule,
