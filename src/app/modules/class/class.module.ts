9c7f69f7ed2d9cc1a687213784b687e0cf4a8cca
Jose Lopez <lopezarizajosealejandro@gmail.com>
2021-04-17 12:49:18 -0500

    Grid View Added

--- src/app/modules/class/class.module.ts
+++ src/app/modules/class/class.module.ts
@@ -1,19 +1,28 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { ClassRoutingModule } from './class-routing.module';
-import { ClassViewComponent } from './components/class-view/class-view.component';
-import { ClassViewHeaderComponent } from './components/class-view-header/class-view-header.component';
-import { ReactiveFormsModule } from '@angular/forms';
 import { SharedModule } from 'app/shared/shared.module';
 
+import { AgGridModule } from 'ag-grid-angular';
+
+import { ClassViewComponent } from './components/class-view/class-view.component';
+import { ClassViewHeaderComponent } from './components/class-view-header/class-view-header.component';
+import { ClassViewGridComponent } from './components/class-view-grid/class-view-grid.component';
+
 @NgModule({
-  declarations: [ClassViewComponent, ClassViewHeaderComponent],
+  declarations: [
+    ClassViewComponent,
+    ClassViewHeaderComponent,
+    ClassViewGridComponent,
+  ],
   imports: [
     CommonModule,
     ClassRoutingModule,
     ReactiveFormsModule,
-    SharedModule
-  ]
+    SharedModule,
+    AgGridModule.withComponents([]),
+  ],
 })
-export class ClassModule { }
+export class ClassModule {}
